74e26f387ad3ea3571db8bc8ae7c83b90db31f45
Vitalij Shitts <vs@vitevic.com>
2018-10-26 18:10:40 +0300

    Diff parser

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vitevic.Shared.Extensions
+{
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> SplitLines(this string input)
+        {
+            return SplitLines(input, Environment.NewLine);
+        }
+
+        public static IEnumerable<string> SplitLines(this string input, string lineEnding)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return Enumerable.Empty<string>();
+
+            string[] lines = input.Split(new[] { lineEnding }, StringSplitOptions.None);
+            return lines.Length == 0 ? Enumerable.Empty<string>() : lines;
+        }
+    }
+}

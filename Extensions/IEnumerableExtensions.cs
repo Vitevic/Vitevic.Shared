386ed0fb4cc181ec1d12b4354dd4ce9995d7bd24
Vitalij Shitts <vs@vitevic.com>
2018-11-13 20:47:39 +0300

    Git log

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitevic.Shared.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this T item)
+        {
+            if (item == null)
+                yield break;
+
+            yield return item;
+        }
+    }
+}
---
+++ NormalizedPathComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Vitevic.Shared.Extensions;
+
+namespace Vitevic.Shared
+{
+    public class NormalizedPathComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string x, string y)
+        {
+            return x.PathEquals(y);
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return obj.NormalizePath().GetHashCode();
+        }
+    }
+}

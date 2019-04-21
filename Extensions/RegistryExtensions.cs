1237e213fa3f85e7f3d51fd05c93a9d29c598312
Vitalij Shitts <vs@vitevic.com>
2019-04-21 14:59:56 +0300

    RegistryExtensions

---
+++ Extensions/RegistryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+
+namespace Vitevic.Shared.Extensions
+{
+    public static class RegistryExtensions
+    {
+        public static RegistryKey Registry32(this RegistryHive registryHive, string subkey)
+        {
+            return RegistryKey.OpenBaseKey(registryHive, RegistryView.Registry32).OpenSubKey(subkey);
+        }
+
+        public static RegistryKey Registry64(this RegistryHive registryHive, string subkey)
+        {
+            return RegistryKey.OpenBaseKey(registryHive, RegistryView.Registry64).OpenSubKey(subkey);
+        }
+    }
+}
